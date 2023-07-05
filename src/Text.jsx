c3b37c1486f4a035be50d8f34b0ee446d2326b35
vumg <ab17100222@gmail.com>
2023-07-05 00:55:27 -0600

    github icon

--- src/Text.jsx
+++ src/Text.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
 import './Text.css';
 
-
 function Text() {
-    return (
-        <div id="text" >
+  return (
+    <div id="text">
+      <div className="text-card text-white text-center p-5 space-y-4 font-plex-mono">
+        <h2 className="text-3xl font-regular">Victor Mendoza</h2>
+        
+        <a href="https://github.com/vumg-z" target="_blank" rel="noopener noreferrer" className="github-link">
+          <AiFillGithub className="github-icon" />
+        </a>
 
-            <div className="text-card text-white text-center p-5 space-y-4 font-plex-mono">
-                <h2 className="text-3xl font-regular">Victor Mendoza</h2>
+        <p className="font-extralight">victormendoza@verlo.co</p>
 
-                {/*  add github icon to this url github.com/vumg-z */}
+        <p className="font-extralight">
+          Front-end developer specialized in interactive websites, <br /> react, 3D visualization, and team leadership.
+        </p>
 
-                <p className='font-extralight'>victormendoza@verlo.co</p>
-
-                <p className="font-extralight">Front-end developer specialized in interactive websites, <br /> react, 3D visualization, and team leadership.</p>
-
-                <div className="flex items-center justify-center space-x-2">
-
-
-                </div>
-            </div>
-        </div>
-    );
+        <div className="flex items-center justify-center space-x-2"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Text;
